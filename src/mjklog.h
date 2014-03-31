8f4b081741cae1de7cff42d75251a3303949ba8c
Michael Keith <mkeith@pulsarastronomy.net>
2014-03-31 18:16:50 +0100

    Log to stderr

--- src/mjklog.h
+++ src/mjklog.h
@@ -3,7 +3,7 @@
  * M.Keith 2012 - let me know if this fails to compile anywhere.
  * mkeith@pulsarastronomy.net
  **/
-#define LOG_OUTFILE stdout
+#define LOG_OUTFILE stderr
 #define WHERESTR  "[%s:%d] "
 #define WHEREARG  __FILE__, __LINE__
 #define ENDL "\n"
